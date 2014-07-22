e82c695efc035d74cdc79e17a66ffce3611b8d91
Stephen Visser <visser.stephen@gmail.com>
2014-07-22 08:54:00 -0600

    Improved algorithm to smoothly transition through phases

--- Conway.playground/section-1.swift
+++ Conway.playground/section-1.swift
@@ -70,53 +70,52 @@ class ConwayRulesEngine {
 class World : SKScene {
     let engine = ConwayRulesEngine()
     var cells = [Point: SKSpriteNode]()
+
+    var dieAction: SKAction!, bornAction: SKAction!
     
     func cellAtCoordinate(coordinate: Point) -> SKSpriteNode {
-        var cell = SKSpriteNode(color: NSColor.blackColor(), size: CGSize(width: 1, height: 1))
-        let point = CGPoint(x: Int(size.width) / 2 + coordinate.x, y: Int(size.height) / 2 + coordinate.y)
-        cell.position = point
+        var cell = SKSpriteNode(color: NSColor(red:68/255.0, green:169/255.0, blue:157/255.0, alpha:0.9), size: CGSize(width: 1, height: 1))
+        cell.position = CGPoint(x: Int(size.width) / 2 + coordinate.x, y: Int(size.height) / 2 + coordinate.y)
         return cell
     }
     
     override func didMoveToView(view: SKView!) {
-        addNodes([Point(x:-2, y: 0), Point(x:-1, y: 0), Point(x:0, y: 0), Point(x:0, y: -1), Point(x:-1, y: -2)], remove:[])
-    }
-    
-    
-    func addNodes(points: [Point], remove: [Point]) {
+        let transitionTime = 0.2
         
-        self.runAction(SKAction.sequence([SKAction.runBlock({
-            for point in remove {
-                if let node = self.cells[point] {
-                    self.cells.removeValueForKey(point)
-                    node.runAction(SKAction.fadeOutWithDuration(0.2))
-                }
-            }
-            
-            for point in points {
-                let view = self.cellAtCoordinate(point)
-                self.cells[point] = view
-                view.alpha = 0.0
-                self.addChild(view)
-                view.runAction(SKAction.fadeInWithDuration(0.2))
-                
-            }
-            
-            }), SKAction.waitForDuration(0.2), SKAction.runBlock({
-                for point in remove {
-                    if let node = self.cells[point] {
-                        self.cells.removeValueForKey(point)
-                        node.runAction(SKAction.removeFromParent())
-                    }
-                }
-                let (born, die) = self.engine.iterate(Array(self.cells.keys))
-                self.addNodes(born, remove: die)
-                })]))
+        dieAction = SKAction.sequence([
+            SKAction.fadeOutWithDuration(transitionTime),
+            SKAction.removeFromParent()
+        ])
+        
+        bornAction = SKAction.fadeInWithDuration(transitionTime)
+        
+        addNodes((born:[Point(x:-2, y: 0), Point(x:-1, y: 0), Point(x:0, y: 0), Point(x:0, y: -1), Point(x:-1, y: -2)], die:[]))
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.2, target: self, selector: Selector("next"), userInfo: nil, repeats: true)
     }
     
+    func next() {
+        self.addNodes(self.engine.iterate(Array(self.cells.keys)))
+    }
+    
+    func addNodes(changes: (born:[Point], die:[Point])) {
+        
+        for point in changes.die {
+            let cell = self.cells[point]!
+            self.cells.removeValueForKey(point)
+            cell.runAction(dieAction)
+        }
+        
+        for point in changes.born {
+            let cell = self.cellAtCoordinate(point)
+            self.cells[point] = cell
+            self.addChild(cell)
+            cell.alpha = 0.0
+            cell.runAction(bornAction)
+        }
+    }
 }
 
-//let toad =
 //let blinker = [Point(x:-1, y: 0), Point(x:0, y: 0), Point(x:1, y: 0)]
 //let beacon = [Point(x:-2, y: -2), Point(x:-1, y: -2), Point(x:-2, y: -1), Point(x:1, y: 1), Point(x:0, y: 1), Point(x:1, y: 0)]
 //let glider = [Point(x:-2, y: 0), Point(x:-1, y: 0), Point(x:0, y: 0), Point(x:0, y: -1), Point(x:-1, y: -2)]
@@ -125,6 +124,6 @@ let view = SKView(frame: CGRect(x:0, y:0, width:200, height:200))
 XCPShowView("View", view)
 
 let world = World(size: CGSize(width:10, height:10))
-world.backgroundColor = NSColor.greenColor()
+world.backgroundColor = NSColor.whiteColor()
 view.presentScene(world)
 
